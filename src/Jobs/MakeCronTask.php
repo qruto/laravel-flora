c2cf012d718b02420ae313ccf0527739408287a9
Vyacheslav Basenko <modern.web.artisan@gmail.com>
2018-02-19 17:01:17 +0200

    For using initializer provided jobs, laravel/framework is required

--- src/Jobs/MakeCronTask.php
+++ src/Jobs/MakeCronTask.php
@@ -4,7 +4,6 @@ namespace MadWeb\Initializer\Jobs;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Contracts\Container\Container;
 
 class MakeCronTask
 {
@@ -15,10 +14,10 @@ class MakeCronTask
      *
      * @return string
      */
-    public function handle(Container $container)
+    public function handle()
     {
-        $app_path = $container->basePath();
-        $task = '* * * * * php '.$app_path.'/artisan schedule:run >> /dev/null 2>&1';
+        $base_path = base_path();
+        $task = '* * * * * php '.$base_path.'/artisan schedule:run >> /dev/null 2>&1';
         exec('(crontab -l 2>/dev/null; echo "'.$task.'") | crontab -');
 
         return 'Base cron task for scheduling work created. Task: '.$task;
