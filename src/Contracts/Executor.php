cb1462b95e3dc4d3409d4a1c777c377a2aaf6e68
Vyacheslav Basenko <modern.web.artisan@gmail.com>
2019-09-15 01:38:13 +0300

    Executor contract сompleted

--- src/Contracts/Executor.php
+++ src/Contracts/Executor.php
@@ -9,4 +9,14 @@ interface Executor
     public function __construct(Command $artisanCommand);
 
     public function exec(array $commands);
+
+    public function artisan(string $command, array $arguments = []);
+
+    public function external(string $command, array $arguments = []);
+
+    public function callable(callable $function, array $arguments = []);
+
+    public function dispatch($job);
+
+    public function dispatchNow($job);
 }
