34962130d111695982b35ebdbd244c0430dd2dec
Slava Razum <razum@qruto.to>
2022-12-25 00:26:25 +0200

    Working with verbose mode

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,17 +5,24 @@ use Qruto\Initializer\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
 
-function chain(?callable $callback = null): object
+function chain(?callable $callback = null, $verbose = false): object
 {
     if ($callback) {
         App::update('testing', $callback);
     }
 
-    return new class
+    return new class($verbose)
     {
-        public function run()
+        public function __construct(protected bool $verbose)
         {
-            return test()->artisan('update');
+        }
+        public function run($options = [])
+        {
+            if (!$this->verbose) {
+                putenv('SHELL_VERBOSITY=0');
+            }
+
+            return test()->artisan('update', $this->verbose ? ['--verbose' => $this->verbose] + $options : $options);
         }
     };
 }
