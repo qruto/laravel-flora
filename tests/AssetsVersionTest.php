229cfa94062820cf49da97996958ff07689a2966
Slava Razum <razum@qruto.to>
2022-12-29 03:38:43 +0200

    wip

---
+++ tests/AssetsVersionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Support\Facades\Cache;
+use Qruto\Initializer\AssetsVersion;
+
+beforeEach(fn () => file_put_contents(
+    base_path('composer.lock'),
+    json_encode(['content-hash' => 'random-hash']
+)));
+
+afterEach(fn () => unlink(base_path('composer.lock')));
+
+test('assets marked as outdated when current and latest hashes are different', function () {
+    $cache = mock(Repository::class)
+        ->shouldReceive('get')
+        ->with('assets_hash')
+        ->andReturn('old-hash')
+        ->getMock();
+
+    app()->instance(Repository::class, $cache);
+
+    $this->assertTrue($this->app->make(AssetsVersion::class)->outdated());
+});
+
+test('assets marked as new when current and latest hashes are same', function () {
+    $cache = mock(Repository::class)
+        ->shouldReceive('get')
+        ->with('assets_hash')
+        ->andReturn('random-hash')
+        ->getMock();
+
+    app()->instance(Repository::class, $cache);
+
+    $this->assertTrue(!$this->app->make(AssetsVersion::class)->outdated());
+});
